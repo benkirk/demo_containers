8ee67e2d5ce241a9ddba4d3cc1534ed2d7a07e3f
Ben Kirk <benkirk@ucar.edu>
2023-03-29 19:34:55 -0600

    cray-mpich in GPUs working

--- mpi/singularity/run_mpi+gpu_container.sh
+++ mpi/singularity/run_mpi+gpu_container.sh
@@ -3,7 +3,7 @@
 #PBS -q main
 #PBS -j oe
 #PBS -l walltime=01:00:00
-#PBS -l select=2:ncpus=128:mpiprocs=32:ompthreads=4
+#PBS -l select=2:ncpus=64:mpiprocs=16:ompthreads=4:ngpus=4
 
 ### Set temp to scratch
 [ -d /glade/gust/scratch/${USER} ] && export TMPDIR=/glade/gust/scratch/${USER}/tmp && mkdir -p $TMPDIR
@@ -15,7 +15,8 @@ status="SUCCESS"
 
 #container_image=./rocky9-mpich-sandbox/
 #container_image=./rocky9-libmesh-sandbox/
-container_image=./opensuse15-openhpc-sandbox/
+#container_image=./opensuse15-openhpc-sandbox/
+container_image=./opensuse15-openhpc-cuda-sandbox/
 
 mkdir -p $(pwd)/mask && echo "Empty Directory to hide container contents." > $(pwd)/mask/README
 
@@ -36,6 +37,8 @@ singularity exec \
         --bind /opt/cray \
         --env LD_LIBRARY_PATH=${CRAY_MPICH_DIR}/lib-abi-mpich:/opt/cray/pe/lib64:${LD_LIBRARY_PATH}:/host/lib64 \
         --env MPICH_SMP_SINGLE_COPY_MODE=NONE \
+        --env MPICH_GPU_SUPPORT_ENABLED=1 \
+        --env MPICH_GPU_MANAGED_MEMORY_SUPPORT_ENABLED=1 \
         ${container_image} \
         ldd /opt/local/osu-micro-benchmarks-7.0.1/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bibw
 
@@ -45,20 +48,112 @@ singularity exec \
 
 
 for exe in \
-    /opt/local/imb-2021.3/IMB-MPI1 \
-    /opt/local/imb-2021.3/IMB-P2P \
-    /opt/local/osu-micro-benchmarks-7.0.1/libexec/osu-micro-benchmarks/mpi/collective/osu_alltoall \
+    /opt/local/osu-micro-benchmarks-7.0.1/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bw \
+    /opt/local/osu-micro-benchmarks-7.0.1/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_bibw \
+    /opt/local/osu-micro-benchmarks-7.0.1/libexec/osu-micro-benchmarks/mpi/pt2pt/osu_latency \
     ; do
 
-    mpiexec --np 64 --ppn 32 --no-transfer \
+    echo && echo && echo "********* Intra-Node-GPU *****************"
+    echo ${exe}
+    mpiexec --np 2 --ppn 2 --no-transfer \
+       get_local_rank \
        singularity exec \
           --bind /run \
           --bind /usr/lib64:/host/lib64 \
           --bind /opt/cray \
           --env LD_LIBRARY_PATH=${CRAY_MPICH_DIR}/lib-abi-mpich:/opt/cray/pe/lib64:${LD_LIBRARY_PATH}:/host/lib64 \
           --env MPICH_SMP_SINGLE_COPY_MODE=NONE \
+          --env MPICH_GPU_SUPPORT_ENABLED=1 \
+          --env MPICH_GPU_MANAGED_MEMORY_SUPPORT_ENABLED=1 \
+          --env LD_PRELOAD=/opt/cray/pe/mpich/8.1.21/gtl/lib/libmpi_gtl_cuda.so.0 \
           ${container_image} \
-          ${exe}
+          ${exe} D D
+
+    echo && echo && echo "********* Inter-Node-GPU *****************"
+    echo ${exe}
+    mpiexec --np 2 --ppn 1 --no-transfer \
+       get_local_rank \
+       singularity exec \
+          --bind /run \
+          --bind /usr/lib64:/host/lib64 \
+          --bind /opt/cray \
+          --env LD_LIBRARY_PATH=${CRAY_MPICH_DIR}/lib-abi-mpich:/opt/cray/pe/lib64:${LD_LIBRARY_PATH}:/host/lib64 \
+          --env MPICH_SMP_SINGLE_COPY_MODE=NONE \
+          --env MPICH_GPU_SUPPORT_ENABLED=1 \
+          --env MPICH_GPU_MANAGED_MEMORY_SUPPORT_ENABLED=1 \
+          --env LD_PRELOAD=/opt/cray/pe/mpich/8.1.21/gtl/lib/libmpi_gtl_cuda.so.0 \
+          ${container_image} \
+          ${exe} D D
+done
+
+
+for exe in \
+    /opt/local/osu-micro-benchmarks-7.0.1/libexec/osu-micro-benchmarks/mpi/collective/osu_alltoall \
+    ; do
+
+    echo && echo && echo "********* Intra-Node-GPU *****************"
+    echo ${exe}
+    mpiexec --np 4 --ppn 4 --no-transfer \
+       get_local_rank \
+       singularity exec \
+          --bind /run \
+          --bind /usr/lib64:/host/lib64 \
+          --bind /opt/cray \
+          --env LD_LIBRARY_PATH=${CRAY_MPICH_DIR}/lib-abi-mpich:/opt/cray/pe/lib64:${LD_LIBRARY_PATH}:/host/lib64 \
+          --env MPICH_SMP_SINGLE_COPY_MODE=NONE \
+          --env MPICH_GPU_SUPPORT_ENABLED=1 \
+          --env MPICH_GPU_MANAGED_MEMORY_SUPPORT_ENABLED=1 \
+          --env LD_PRELOAD=/opt/cray/pe/mpich/8.1.21/gtl/lib/libmpi_gtl_cuda.so.0 \
+          ${container_image} \
+          ${exe} -d managed
+
+    echo && echo && echo "********* Intra-Node-GPU *****************"
+    echo ${exe}
+    mpiexec --np 16 --ppn 16 --no-transfer \
+       get_local_rank \
+       singularity exec \
+          --bind /run \
+          --bind /usr/lib64:/host/lib64 \
+          --bind /opt/cray \
+          --env LD_LIBRARY_PATH=${CRAY_MPICH_DIR}/lib-abi-mpich:/opt/cray/pe/lib64:${LD_LIBRARY_PATH}:/host/lib64 \
+          --env MPICH_SMP_SINGLE_COPY_MODE=NONE \
+          --env MPICH_GPU_SUPPORT_ENABLED=1 \
+          --env MPICH_GPU_MANAGED_MEMORY_SUPPORT_ENABLED=1 \
+          --env LD_PRELOAD=/opt/cray/pe/mpich/8.1.21/gtl/lib/libmpi_gtl_cuda.so.0 \
+          ${container_image} \
+          ${exe} -d managed
+
+    echo && echo && echo "********* Inter-Node-GPU *****************"
+    echo ${exe}
+    mpiexec --np 8 --ppn 4 --no-transfer \
+       get_local_rank \
+       singularity exec \
+          --bind /run \
+          --bind /usr/lib64:/host/lib64 \
+          --bind /opt/cray \
+          --env LD_LIBRARY_PATH=${CRAY_MPICH_DIR}/lib-abi-mpich:/opt/cray/pe/lib64:${LD_LIBRARY_PATH}:/host/lib64 \
+          --env MPICH_SMP_SINGLE_COPY_MODE=NONE \
+          --env MPICH_GPU_SUPPORT_ENABLED=1 \
+          --env MPICH_GPU_MANAGED_MEMORY_SUPPORT_ENABLED=1 \
+          --env LD_PRELOAD=/opt/cray/pe/mpich/8.1.21/gtl/lib/libmpi_gtl_cuda.so.0 \
+          ${container_image} \
+          ${exe} -d managed
+
+    echo && echo && echo "********* Inter-Node-GPU *****************"
+    echo ${exe}
+    mpiexec --np 32 --ppn 16 --no-transfer \
+       get_local_rank \
+       singularity exec \
+          --bind /run \
+          --bind /usr/lib64:/host/lib64 \
+          --bind /opt/cray \
+          --env LD_LIBRARY_PATH=${CRAY_MPICH_DIR}/lib-abi-mpich:/opt/cray/pe/lib64:${LD_LIBRARY_PATH}:/host/lib64 \
+          --env MPICH_SMP_SINGLE_COPY_MODE=NONE \
+          --env MPICH_GPU_SUPPORT_ENABLED=1 \
+          --env MPICH_GPU_MANAGED_MEMORY_SUPPORT_ENABLED=1 \
+          --env LD_PRELOAD=/opt/cray/pe/mpich/8.1.21/gtl/lib/libmpi_gtl_cuda.so.0 \
+          ${container_image} \
+          ${exe} -d managed
 done
 
 echo && echo && echo ${status} $(date)
